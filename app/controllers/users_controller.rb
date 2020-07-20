008beac3dac3aa079c5064aee05f62d86e558200
meganeJPN <ooosamuuu1985@gmail.com>
2020-07-20 11:46:59 +0900

    サインアップ後、自動でログインしてトップページに遷移するように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      session[:user_id] = @user.id
+      redirect_to poeets_path
     else
       render :new
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
