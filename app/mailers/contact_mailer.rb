51ecca1943cc62e5791c4eed6d4c022a79503070
meganeJPN <ooosamuuu1985@gmail.com>
2020-07-22 15:43:36 +0900

    メールのサブジェクトの修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(poeet)
     @poeet = poeet
-    mail to: "ooosamuuu1985@gmail.com", subject: "お問い合わせの確認メール"
+    mail to: "ooosamuuu1985@gmail.com", subject: "新規ポイート"
   end
 end
